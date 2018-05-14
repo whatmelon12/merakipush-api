4c8f011626037c3dddee0b9a6ca1cb564b9a7bad
Omar Mejia <omargotico@hotmail.com>
2018-05-14 13:09:59 -0500

    Added body parser middleware to express

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DB_MONGO);
+mongoose.connect(process.env.DB_MONGO_PROD);
 
 module.exports = {mongoose};
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const merakiRoutes = require('./routes/merakiRoutes');
 
 const app = express();
+app.use(bodyParser.json());
 const port = process.env.PORT || 3000;
 
 merakiRoutes(app);
